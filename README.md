1b2d1e1d2fb73ff837996b45de40adf938c74275
Aditya Raj <rajaditya27145@gmail.com>
2024-02-16 19:00:06 +0530

    🎉 working v1 with static data

--- README.md
+++ README.md
@@ -28,3 +28,10 @@ export default {
 - Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
 - Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
 - Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+
+// Two ways to render infinite grids:
+// 1. Call an api to load containers with required info of the aspect ratio and image url to pull inside them.
+// 2. Load the UI only after receieving enough batch of image (say 10 is required in one viewport then let the 10 images load and then render the UI) and show a good loading skeleton till then.
+
+// Pre-requisite before thinking about rendering:
+// Use Cloudinary or Nextjs Image to have on the fly transformation and caching enabled on images to help load faster whatever the scenario.
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,151 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Fragment, useState } from "react";
+import { useWindowSize } from "@react-hook/window-size";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [items, setItems] = useState(initialItems);
+  const [width] = useWindowSize();
+
+  const numberOfColumns =
+    width && width < 420
+      ? 1
+      : width && width > 420 && width < 768
+      ? 2
+      : width && width > 768 && width < 1024
+      ? 3
+      : 4;
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <InfiniteScroll
+        dataLength={items.length}
+        next={() =>
+          setTimeout(() => setItems([...items, ...initialItems]), 5000)
+        }
+        hasMore={true}
+        loader={
+          <h4 className="pl-4 pb-4 text-lg font-semibold text-gray-500 w-full text-center">
+            Loading...
+          </h4>
+        }
+      >
+        <div className="flex place-content-stretch gap-4 flex-wrap p-4">
+          {[...Array(numberOfColumns)].map((_, index) => (
+            <div
+              key={index}
+              className="flex flex-col place-content-stretch flex-1 space-y-4"
+            >
+              {items.map((item, i) => (
+                <Fragment key={item.id}>
+                  {i % numberOfColumns === index && (
+                    <Grid key={item.id} item={item} />
+                  )}
+                </Fragment>
+              ))}
+            </div>
+          ))}
+        </div>
+      </InfiniteScroll>
     </>
-  )
+  );
+};
+
+const Grid = ({ item }: { item: Item }) => {
+  return (
+    <div className="w-full h-full">
+      <img
+        src={item.img}
+        alt=""
+        className="w-full h-full object-contain overflow-hidden rounded-md"
+      />
+    </div>
+  );
+};
+
+interface Item {
+  id: number;
+  img: string;
 }
 
-export default App
+const initialItems = [
+  {
+    id: 1,
+    img: "https://storage.googleapis.com/pai-images/a4c6e61a50774541ad5b97223b82b74b.jpeg",
+  },
+  {
+    id: 2,
+    img: "https://storage.googleapis.com/pai-images/b055936d4fed41a8882dba086ef2a48b.jpeg",
+  },
+  {
+    id: 3,
+    img: "https://storage.googleapis.com/pai-images/e49f2e45a6b64270afb2a07b932844e9.jpeg",
+  },
+  {
+    id: 4,
+    img: "https://storage.googleapis.com/pai-images/9b796344d52840e9a9062c6d33d9ad50.jpeg",
+  },
+  {
+    id: 5,
+    img: "https://storage.googleapis.com/pai-images/789104cdaea14825b8ba1c45436fb638.jpeg",
+  },
+  {
+    id: 6,
+    img: "https://storage.googleapis.com/pai-images/a4c6e61a50774541ad5b97223b82b74b.jpeg",
+  },
+  {
+    id: 7,
+    img: "https://storage.googleapis.com/pai-images/b055936d4fed41a8882dba086ef2a48b.jpeg",
+  },
+  {
+    id: 8,
+    img: "https://storage.googleapis.com/pai-images/e49f2e45a6b64270afb2a07b932844e9.jpeg",
+  },
+  {
+    id: 9,
+    img: "https://storage.googleapis.com/pai-images/9b796344d52840e9a9062c6d33d9ad50.jpeg",
+  },
+  {
+    id: 10,
+    img: "https://storage.googleapis.com/pai-images/789104cdaea14825b8ba1c45436fb638.jpeg",
+  },
+  {
+    id: 11,
+    img: "https://storage.googleapis.com/pai-images/a4c6e61a50774541ad5b97223b82b74b.jpeg",
+  },
+  {
+    id: 12,
+    img: "https://storage.googleapis.com/pai-images/b055936d4fed41a8882dba086ef2a48b.jpeg",
+  },
+  {
+    id: 13,
+    img: "https://storage.googleapis.com/pai-images/e49f2e45a6b64270afb2a07b932844e9.jpeg",
+  },
+  {
+    id: 14,
+    img: "https://storage.googleapis.com/pai-images/9b796344d52840e9a9062c6d33d9ad50.jpeg",
+  },
+  {
+    id: 15,
+    img: "https://storage.googleapis.com/pai-images/789104cdaea14825b8ba1c45436fb638.jpeg",
+  },
+  {
+    id: 16,
+    img: "https://storage.googleapis.com/pai-images/a4c6e61a50774541ad5b97223b82b74b.jpeg",
+  },
+  {
+    id: 17,
+    img: "https://storage.googleapis.com/pai-images/b055936d4fed41a8882dba086ef2a48b.jpeg",
+  },
+  {
+    id: 18,
+    img: "https://storage.googleapis.com/pai-images/e49f2e45a6b64270afb2a07b932844e9.jpeg",
+  },
+  {
+    id: 19,
+    img: "https://storage.googleapis.com/pai-images/9b796344d52840e9a9062c6d33d9ad50.jpeg",
+  },
+  {
+    id: 20,
+    img: "https://storage.googleapis.com/pai-images/789104cdaea14825b8ba1c45436fb638.jpeg",
+  },
+];
